64e06b884981f4608054e66e4bd7bb9d049ffea1
Léo Salé <leo.sale72@gmail.com>
2020-09-30 20:00:55 +0200

    add searchbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import MeteoCard from "./MeteoCard";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <Grid container direction="column" justify="center" alignItems="center">
       <h1>MeteoApp</h1>
+      <SearchBar />
       <MeteoCard />
     </Grid>
   );
